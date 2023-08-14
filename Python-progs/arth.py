3b406690a08b699adf06ad5bf7ed365c7e544a13
charan raj <charanofficial30@gmail.com>
2023-08-14 16:00:50 +0530

    Division method added

--- Python-progs/arth.py
+++ Python-progs/arth.py
@@ -2,8 +2,11 @@ def add(n1,n2):
     return(n1+n2)
 def mul(n1,n2):
     return(n1*n2)
+def div(n1,n2):
+    return(n1/n2)
 
 a=int(input("Enter num1 :"))
 b=int(input("Enter num2 :"))
 print("sum=",add(a,b))
 print("sum=",mul(a,b))
+print("sum=",div(a,b))
